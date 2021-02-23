efe8ab9e1669bcdba3d0f8e19c62cc4a9bb4bccb
natalia.palota <natalia.palota@zup.com.br>
2021-02-23 16:20:46 -0300

    Criar método para executar o programa

--- src/com/br/zup/Biblioteca.java
+++ src/com/br/zup/Biblioteca.java
@@ -10,10 +10,32 @@ public class Biblioteca {
     }
 
     private void menu (){
+
+        IO.output("Opção 1: Cadastrar nome do livro\n" +
+                "Opção 2: Listar os livros\n" +
+                "Opção 3: Buscar o livro por autor\n" +
+                "Opção 4: Buscar o livro por editora\n" +
+                "Opção 0: Sair do programa");
+    }
+
+    public void executar (){
         IO.output("Bem vindo a biblioteca!");
-        IO.output("Opção 1: cadastrar nome do livro\n" +
-                "Opção 2: listar os livros\n" +
-                "Opção 3: buscar o livro por autor\n" +
-                "Opção 4: buscar o livro por editora ");
+        while (executar){
+            menu();
+            int option = IO.input().nextInt();
+            if(option == 1){
+                IO.output("Por favor, forneça as seguintes informações: autor, título, editora, número de exemplares e categoria");
+               InformacoesDoLivro livro =  servicoLivros.cadastrarNovoLivro(
+                        IO.input().nextLine(),
+                        IO.input().nextLine(),
+                        IO.input().nextLine(),
+                        IO.input().nextInt(),
+                        IO.input().nextLine()
+                );
+               IO.output("Livro foi cadastrado " + livro.toString());
+            }
+
+        }
+
     }
 }
