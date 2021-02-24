92b365a9f0f0445f9096a5f415315a203aa2587e
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-24 14:36:56 -0300

    Criar método para buscar o usuário para cadastrar os livros

--- src/com/br/zup/ServicoUsuario.java
+++ src/com/br/zup/ServicoUsuario.java
@@ -11,8 +11,23 @@ public class ServicoUsuario {
             Usuario usuario = new Usuario(nome,email);
             usuarios.add(usuario);
             return usuario;
-        }else{ throw new Exception("email não é válido"); }
+        } else {
+            throw new Exception("email não é válido");
+        }
+    }
 
+    public Usuario pesquisarUsuarioPorEmail(String email) throws Exception {
+        Usuario usuario = null;
+        for (Usuario item : usuarios) {
+            if (item.getEmail().equalsIgnoreCase(email)) {
+                usuario = item;
+            }
+        }
+        if (usuario != null) {
+            return usuario;
+        } else {
+            throw new Exception("Usuário não localizado, portanto, não será possível cadastrar livros!");
+        }
     }
 }
 //Cadastrar um novo usuáiro:
