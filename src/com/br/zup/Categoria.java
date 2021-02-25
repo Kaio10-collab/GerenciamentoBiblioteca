75eab0e1eeca848ddd99cf7d380621dd21a66f44
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-25 13:20:14 -0300

    Criar Enum de categoria de livros

--- src/com/br/zup/Categoria.java
+++ src/com/br/zup/Categoria.java
@@ -1,2 +1,13 @@
-package com.br.zup;public enum Categoria {
+package com.br.zup;
+
+public enum Categoria {
+    ROMANCE,
+    FICCAO_CIENTIFICA,
+    TI,
+    ACAO,
+    COMEDIA,
+    ARTE,
+    DOCUMENTARIO,
+    BIOGRAFIA,
+    CLASSICO
 }
